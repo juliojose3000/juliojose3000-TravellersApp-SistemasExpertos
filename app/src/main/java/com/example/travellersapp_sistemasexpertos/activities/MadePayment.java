2e6dd2b4bb835df85ac33717c3a91f92bc533fac
Allan Solano <ajosue19@gmail.com>
2020-03-26 13:56:53 -0600

    Funcionalidad de Correo

--- app/src/main/java/com/example/travellersapp_sistemasexpertos/activities/MadePayment.java
+++ app/src/main/java/com/example/travellersapp_sistemasexpertos/activities/MadePayment.java
@@ -3,14 +3,22 @@ package com.example.travellersapp_sistemasexpertos.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.travellersapp_sistemasexpertos.MainActivity;
 import com.example.travellersapp_sistemasexpertos.R;
+import com.example.travellersapp_sistemasexpertos.database.DBHelper;
 import com.example.travellersapp_sistemasexpertos.database.Data;
 import com.example.travellersapp_sistemasexpertos.domain.TravelPackage;
+import com.example.travellersapp_sistemasexpertos.domain.User;
+import com.example.travellersapp_sistemasexpertos.utilities.SentMail;
+
+import java.io.IOException;
 
 public class MadePayment extends BaseActivity {
 
@@ -21,6 +29,7 @@ public class MadePayment extends BaseActivity {
     TextView textViewPaymentDate;
     TextView textViewDateStart;
     TextView textViewDateEnd;
+    boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +41,23 @@ public class MadePayment extends BaseActivity {
         Bundle bundle = getIntent().getExtras();
 
         int idPackage = bundle.getInt("idTravelPackage");
-
         TravelPackage travelPackage = Data.getTravelPackageById(idPackage);
-
+        flag = false;
         textViewUser = findViewById(R.id.textView_user);
 
-        textViewUser.setText("Cliente: "+Data.loggedUser.getName()+" "+Data.loggedUser.getLastName());
+        textViewUser.setText("Cliente: " + Data.loggedUser.getName() + " " + Data.loggedUser.getLastName());
 
         textViewPrice = findViewById(R.id.textView_price);
 
-        textViewPrice.setText("Pago: $"+travelPackage.getCost());
+        textViewPrice.setText("Pago: $" + travelPackage.getCost());
 
         textViewAirport = findViewById(R.id.textView_airport);
 
-        textViewAirport.setText("Aeropuerto: "+travelPackage.getAirport().getName());
+        textViewAirport.setText("Aeropuerto: " + travelPackage.getAirport().getName());
 
         textViewHotel = findViewById(R.id.textView_hotel);
 
-        textViewHotel.setText("Hotel: "+travelPackage.getHotel().getName());
+        textViewHotel.setText("Hotel: " + travelPackage.getHotel().getName());
 
         textViewPaymentDate = findViewById(R.id.textView_payment_date);
 
@@ -57,16 +65,16 @@ public class MadePayment extends BaseActivity {
 
         textViewDateStart = findViewById(R.id.textView_date_start);
 
-        textViewDateStart.setText("Desde el: "+travelPackage.getStartDate().toString());
+        textViewDateStart.setText("Desde el: " + travelPackage.getStartDate().toString());
 
         textViewDateEnd = findViewById(R.id.textView_date_end);
 
-        textViewDateEnd.setText("Hasta el: "+travelPackage.getEndDate().toString());
+        textViewDateEnd.setText("Hasta el: " + travelPackage.getEndDate().toString());
 
     }
 
 
-    public void finish(View v){
+    public void finish(View v) {
 
         Intent i = new Intent(this, MainInterface.class);
 
@@ -83,11 +91,47 @@ public class MadePayment extends BaseActivity {
     }
 
 
-    public void sendEmail(View v){
+    public void sendEmail(View v) {
+        final User loggedUser = Data.loggedUser;
 
-        Toast.makeText(this,"Enviando correo...",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Enviando correo...", Toast.LENGTH_LONG).show();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                SentMail mail = new SentMail("ajosue19.as@gmail.com", "batistuta27!");
+
+                String[] toArr = {loggedUser.getMail()};
+                mail.set_to(toArr);
+                mail.set_from("ajosue19@gmail.com");
+                mail.set_subject("Confirmación de reserva de viaje");
+                mail.setBody("Su reservación se ha completado con éxito, disfrute de su experiencia " +
+                        "con World Travel");
+
+                try {
+                    // mail.addAttachment("/sdcard/filelocation");
+
+                    if (mail.send()) {
+                        flag = true;
+                    }
+                } catch (Exception e) {
+                    //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+                    Log.e("MailApp", "No se pudo enviar el correo", e);
+                }
+
+            }
+
+        });
 
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
